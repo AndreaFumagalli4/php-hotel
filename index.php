59c636375e44b791010e4e0985768eccb2ebd15b
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-01-25 15:12:48 +0100

    print all hotel's data

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>  
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,8 +47,21 @@
         <title>PHP Hotel</title>
     </head>
     <body>
-        <h1>
-        PHP Hotel
-        </h1>
+        <header>
+            <h1>
+            PHP Hotel
+            </h1>
+        </header>
+        <main>
+            <pre>
+                <?php
+                    foreach($hotels as $hotel){
+                        foreach($hotel as $hotelEl){
+                            var_dump($hotelEl);
+                        }
+                    }
+                ?>
+            </pre>
+        </main>
     </body>
 </html>
